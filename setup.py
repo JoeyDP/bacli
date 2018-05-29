0f6b787760e51d27a197d017fb59ce74233db6ef
Joey De Pauw <joeydepauw@gmail.com>
2018-05-29 13:15:04 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='bacli',
-    packages=['bacli'],
-    version='0.1',
-    description='Born Again Command Line Interface.',
-    author='Joey De Pauw',
-    author_email='joeydepauw@gmail.com',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bacli",
+    version="0.1",
+    author="Joey De Pauw",
+    author_email="joeydepauw@gmail.com",
+    description="Born Again Command Line Interface.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/JoeyDP/bacli',
-    # download_url = '',
-    keywords=['cli', 'command', 'argument', 'parsing'],
-    classifiers=[],
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
